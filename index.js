80b522830917010e66c888cd92d5a7e0256845a6
Raquel <raquel.chacim@hotmail.com>
2022-11-01 11:50:29 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+  console.log(`This Saturday I want to ${fun}!`);
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(work = " I will go to the office") {
+  console.log(`This Monday, ${work}!`);
+}
+mondayWork("I will work from home");
+
+let wrapAdjective = "*";
+let result = "a hard worker";
+function compliment() {
+  console.log(`You are ${wrapAdjective} ${result} ${wrapAdjective}`);
+}
+compliment();
+
+let wrapAdjective = "||";
+let result = "a dedicated programmer";
+function compliment() {
+  console.log(`You are ${wrapAdjective} ${result} ${wrapAdjective}`);
+}
+compliment();
